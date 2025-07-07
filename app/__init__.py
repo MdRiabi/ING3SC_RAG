7900c967cee5ee587436a27d9bf341a1fa3dc2a1
MdRiabi <mdriabi@gmail.com>
2025-07-07 08:19:05 +0100

    Update run configuration to allow external access and add debug print statements in create_app for better visibility during initialization

--- app/__init__.py
+++ app/__init__.py
@@ -9,12 +9,15 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app():
+    print(">>> Début de create_app")
     app = Flask(__name__)
     app.config.from_object(Config)
+    print(">>> Flask et config OK")
     
     # Initialize extensions
     db.init_app(app)
     login_manager.init_app(app)
+    print(">>> Extensions OK")
     
     
     # Register blueprints
@@ -24,10 +27,12 @@ def create_app():
     app.register_blueprint(main)
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(chat_bp, url_prefix='/chat')
+    print(">>> Blueprints enregistrés")
     
     # Create tables
     with app.app_context():
         db.create_all()
+    print(">>> Tables créées")
     
     return app
 
